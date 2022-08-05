133799e1b63fe2e5ee6ddb4a7bdfbcf143009d83
Nguyễn Lưu Thường <luuthuong20022000@gmail.com>
2022-08-05 16:23:55 +0700

    configure structure backend

---
+++ Repositories/RepositoryBase.cs
@@ -0,0 +1,44 @@
+﻿using Entities;
+using Repositories.Interface;
+using System.Linq.Expressions;
+
+namespace Repositories
+{
+    public class RepositoryBase<T,Context> : IRepositoryBase<T>
+    {
+        private readonly IdentityUserContext _context;
+        public RepositoryBase(Context context)
+        {
+            _context = context;
+        }
+        public bool Create(T entity)
+        {
+
+        }
+
+        public bool Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetByCondition(Expression<Func<T, bool>> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
